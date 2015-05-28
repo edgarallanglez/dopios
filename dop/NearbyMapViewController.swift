a56d73c71074d9c8b009c85e8f5e5afcde9cba17
edgarallang <edgarallan182@gmail.com>
2015-05-28 17:27:18 -0600

    tercer item

---
+++ dop/NearbyMapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NearbyMapViewController.swift
+//  dop
+//
+//  Created by Edgar Allan Glez on 5/28/15.
+//  Copyright (c) 2015 Edgar Allan Glez. All rights reserved.
+//
+
+import Foundation
+
+class NearbyMapViewController: UIViewController {
+    
+}
