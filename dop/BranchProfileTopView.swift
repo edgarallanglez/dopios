91f502d4321ba280199a9db57a434e34e19aaa26
edgarallang <edgarallan182@gmail.com>
2015-08-06 17:58:52 -0600

    branch profile about view

---
+++ dop/BranchProfileTopView.swift
@@ -0,0 +1,17 @@
+//
+//  BranchProfileTopView.swift
+//  dop
+//
+//  Created by Edgar Allan Glez on 8/6/15.
+//  Copyright (c) 2015 Edgar Allan Glez. All rights reserved.
+//
+
+import UIKit
+
+class BranchProfileTopView: UIView {
+    
+    @IBOutlet weak var branchCover: UIImageView!
+    @IBOutlet weak var branchLogo: UIImageView!
+    @IBOutlet weak var branchProfileSegmented: UISegmentedControl!
+    
+}
