df243fa06496d9c10dc82d26d8d563b0dde34a4b
jequintero <eduardo.quintero52@gmail.com>
2015-08-18 15:23:27 -0600

    coupon controller blocks

--- dop/CouponController.swift
+++ dop/CouponController.swift
@@ -10,21 +10,24 @@ class CouponController {
     typealias ServiceResponse = (NSDictionary?, NSError?) -> Void
 
   
-    class func getAllCouponsWithSuccess(success: ((couponsData: NSData!) -> Void)) {
-        
+    class func getAllCouponsWithSuccess(success succeed: ((couponsData: NSData!) -> Void),failure errorFound: ((couponsData: NSError?) -> Void)) {
         let url = "\(Utilities.dopURL)coupon/all/get/user"
         Utilities.loadDataFromURL(NSURL(string: url)!, completion:{(data, error) -> Void in
             if let urlData = data {
-                success(couponsData: urlData)
+                succeed(couponsData: urlData)
+            }else{
+                errorFound(couponsData: error)
             }
         })
     }
     
-    class func takeCouponWithSuccess(params:[String:AnyObject], success: ((couponsData: NSData!) -> Void)) {
+    class func takeCouponWithSuccess(params:[String:AnyObject], success succeed: ((couponsData: NSData!) -> Void) ,failure errorFound: ((couponsData: NSError?) -> Void)){
         let url = "\(Utilities.dopURL)coupon/user/take"
         Utilities.sendDataToURL(NSURL(string: url)!, method:"POST", params: params, completion:{(data, error) -> Void in
             if let urlData = data {
-                success(couponsData: urlData)
+                succeed(couponsData: urlData)
+            }else{
+                errorFound(couponsData: error)
             }
         })
     }
