980ceae6b9f032f37409361915215683a5eacf21
edgarallang <edgarallan182@gmail.com>
2015-08-20 10:54:17 -0600

    clean to pull and then push

---
+++ dop/MoreMenuController.swift
@@ -0,0 +1,9 @@
+//
+//  MoreMenuController.swift
+//  dop
+//
+//  Created by Edgar Allan Glez on 8/20/15.
+//  Copyright (c) 2015 Edgar Allan Glez. All rights reserved.
+//
+
+import Foundation
---
+++ dop/MoreMenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MoreMenuViewController.swift
+//  dop
+//
+//  Created by Edgar Allan Glez on 8/20/15.
+//  Copyright (c) 2015 Edgar Allan Glez. All rights reserved.
+//
+
+import UIKit
+
+class MoreMenuViewController: UIViewController {
+    
+}
